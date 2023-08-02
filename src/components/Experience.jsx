856fc1ec3ee71e837569f283e499cdf30966a902
EshuShango <tobiakinyemi21@gmail.com>
2023-08-02 00:59:02 -0500

    first init

---
+++ src/components/Experience.jsx
@@ -0,0 +1,15 @@
+import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls  />
+       <group position={[0, -1, 0]}>
+
+      <Avatar />  
+      </group>
+      <ambientLight intensity={3.14} />
+    </>
+  );
+};
