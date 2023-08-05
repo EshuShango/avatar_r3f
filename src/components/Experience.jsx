6d836972f059009fbca573affb73ffb46b6a8776
EshuShango <tobiakinyemi21@gmail.com>
2023-08-05 05:37:24 -0500

    need to fix bug

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,24 @@
+/* eslint-disable react/no-unknown-property */
 import { OrbitControls } from "@react-three/drei";
 import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+    value: "Sitting",
+    options: ["Sitting", "Typing", "Falling", "Standing", "Looking"],
+    },
+  });
   return (
     <>
       <OrbitControls  />
-       <group position={[0, -1, 0]}>
+       <group position-y={-1}>
 
-      <Avatar />  
+      <Avatar animation={animation}/>  
       </group>
       <ambientLight intensity={3.14} />
     </>
   );
 };
+//! Something is wrong with the model. It is not loading correctly.
